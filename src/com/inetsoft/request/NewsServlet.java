c3192ef3ed494c0ec87f3ac0847751df5bc6e652
DreamLi <18829346477@163.com>
2017-12-19 23:19:13 +0800

    after sendredirect should return

--- src/com/inetsoft/request/NewsServlet.java
+++ src/com/inetsoft/request/NewsServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/NewsServlet")
 public class NewsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
@@ -18,6 +17,7 @@ public class NewsServlet extends HttpServlet {
 		String referer = request.getHeader("Referer");
 		if(referer == null || "".equals(referer) || !referer.contains("localhost/")) {
 			response.sendRedirect(request.getContextPath() + "/index.jsp");
+			return;
 		}
 		
 		response.getWriter().write("习大大吃包子....");
@@ -26,5 +26,4 @@ public class NewsServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
